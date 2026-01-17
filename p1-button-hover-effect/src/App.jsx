7a8722ce7958aea2d8ba5f93205c46b04dec4ec4
Karthik J Nair <you@example.com>
2026-01-17 11:04:00 +0530

    updated button hover

--- p1-button-hover-effect/src/App.jsx
+++ p1-button-hover-effect/src/App.jsx
@@ -3,9 +3,11 @@ import React from "react";
 const App = () => {
   return (
     <div className="h-screen bg-zinc-900 flex justify-center items-center">
-      <button className="relative group rounded-lg pl-10 pt-1 pb-1 pr-3 text-xl font-semibold flex items-center gap-2 text-zinc-50 cursor-pointer border hover:bg-amber-600 transition-all duration-500">
+      <button className="relative group rounded-lg pl-10 py-0 pr-3 text-xl font-semibold flex items-center gap-2 text-zinc-50 cursor-pointer border   transition-all duration-500">
         <Box />
-        <span className="inline-block group-hover:-translate-x-8 transition-all duration-500">Chat with Karthik</span>
+        <div className="absolute inset-0 bg-white/20 rounded-[7px] [clip-path:inset(0_100%_0_0)] group-hover:[clip-path:inset(0_0%_0_0)] transition-[clip-path] duration-500"/>
+
+        <span className="inline-block group-hover:-translate-x-8 transition-all duration-500 uppercase">Karthik</span>
       </button>
     </div>
   );
@@ -15,7 +17,7 @@ export default App;
 
 const Box = () => {
   return (
-    <div className="size-8 absolute group-hover:left-[calc(100%-2.11rem)] left-0.5 my-auto inset-y-0 group-hover:transform group-hover:rotate-180 flex rounded-md transition-all duration-500 bg-amber-600 flex-col justify-center gap-px items-center">
+    <div className="size-7 absolute z-20 group-hover:left-[calc(100%-1.749rem)] left-0 my-auto inset-y-0 group-hover:transform group-hover:rotate-180 flex rounded-[7px] transition-all duration-500 bg-amber-600 flex-col justify-center gap-px items-center">
       <div className="flex gap-px">
         <Bubble />
         <Bubble />
@@ -58,7 +60,7 @@ const Box = () => {
 const Bubble = ({ highlight = false }) => {
   return (
     <span
-      className={`inline-block size-1  rounded-full shrink-0 bg-white/25 ${
+      className={`inline-block size-1 shrink-0 bg-white/25 ${
         highlight ? "bg-white/90 animate-pulse ease-linear duration-50" : "bg-zinc-900/10"
       }`}
     />
